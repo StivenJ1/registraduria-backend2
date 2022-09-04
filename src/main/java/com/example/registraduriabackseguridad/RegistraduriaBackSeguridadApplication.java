9b8a7a9ff3c76b003ffc471c2b4dc9ecec054a0f
Stiven Joel <stivenu.joelb@konradlorenz.edu.co>
2022-09-03 20:49:58 -0500

    primer commit con las dependecias de maven

---
+++ src/main/java/com/example/registraduriabackseguridad/RegistraduriaBackSeguridadApplication.java
@@ -0,0 +1,13 @@
+package com.example.registraduriabackseguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistraduriaBackSeguridadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistraduriaBackSeguridadApplication.class, args);
+    }
+
+}
